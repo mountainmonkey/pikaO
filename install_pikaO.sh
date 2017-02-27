877fd27c19f520848090d0d79b1b5ec36f66bba5
mountain.monkey <felix.wu@gmail.com>
2017-02-27 12:40:17 -0800

    initial call of Pika!

---
+++ install_pikaO.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+
+VERSION=0
+
+if grep -q -s -i "release 7" /etc/redhat-release ; then
+    VERSION="7"
+elif grep -q -s -i "release 6" /etc/redhat-release ; then
+    VERSION="6"
+else
+    echo "This script is written for RHEL & CentOS 6/7 only, sorry";
+    exit 9;
+fi
+
+groupadd -g 6999 pika ;
+useradd -u 6999 -g 6999 pika ;
+
+if [ "$VERSION" -eq "7" ] ; then
+    curl https://raw.githubusercontent.com/mountainmonkey/pikaO/master/pikaO7.sh -O /home/pika/pikaO.sh
+elif [ "$VERSION" -eq "6" ] ; then
+    curl https://raw.githubusercontent.com/mountainmonkey/pikaO/master/pikaO6.sh -O /home/pika/pikaO.sh
+fi
+
+chmod 700 /home/pika/pikaO.sh
+chown pika: /home/pika/pikaO.sh
+
+#if ! ping -q -c 1 -W 1 www.google.com > /dev/null ; then
+#    echo "This machine cannot reach internet, using internal SMTP server..."
+#    sed -i s/"SMTP=''"/"SMTP=\'-S smtp=smtp.talusfield.home\'"/g /home/pika/pika.sh
+#fi
+
+cat > /var/spool/cron/pika << EOF
+*/10 * * * * /home/pika/pikaO.sh
+EOF
+
+chmod 600 /var/spool/cron/pika
+chown pika: /var/spool/cron/pika
+
+su pika -c "/home/pika/pikaO.sh -v"
+su pika -c "crontab -l"
+echo "pikaO is installed! remember to edit /home/pika/pika.sh to change the email address and calling threshold as you like!";
---
+++ pikaO7.sh
@@ -0,0 +1,74 @@
+###############################################################################
+# pikaO
+# RHEL/CentOS 7 Edition
+# Version 1.3
+# Mon Feb 27 10:57:33 PST 2017
+# Felix Wu
+# pika"O" is a poor man's monitoring script, it checks basic CPU, RAM and 
+# root partition usage.
+# When this is run by a cron job, it continue to send spam emails until it goes
+# under the threshold. 
+# O represents pika open its mouth making a warning call on constant intervals. 
+###############################################################################
+
+#!/bin/bash
+
+THCPUUSED=50
+THDISKUSED=80
+THMEMFREE=20
+
+MAILTO=spamme@talusfield.home
+MAILFROM=pika.dontcallme@`hostname`
+SMTP=''
+
+# disk
+DISK=`df / | grep "% /" | awk '{ print $(NF-1)}' | sed 's/%//g'`
+
+#cpu
+CPU=`vmstat 1 2 -a | tail -1 | awk '{print $13+$14}'`
+
+# mem
+# RHEL7 & CentOS7 only
+MEM=`free | grep Mem | awk '{ printf ("%.0f\n",($NF/$2)*100) }'`
+
+function sysstat() {
+    printf "CPU: %s%% used\n" "$CPU"
+    printf "Memory: %s%% free\n" "$MEM"
+    printf "root partition: %s%% used\n" "$DISK"
+    vmstat 1 2 -a
+    echo "--------------------------------------------------------------------------------"
+    df -h /
+    echo "--------------------------------------------------------------------------------"
+    free -h
+    echo "--------------------------------------------------------------------------------"
+    eval $1
+    echo "--------------------------------------------------------------------------------"
+}
+
+if [ "$1" = "-v" ] ; then
+    printf "[pikaO] is running in verbose mode...\n"
+    sysstat "top -b -n 1 -o %MEM" | head -30
+    exit 1;
+fi
+
+if [ "$1" = "-t" ] ; then
+    printf "[pikaO] is running in test mode to send an alert email...\n"
+    SYSSTAT=`sysstat "top -b -n 1 -o %MEM"`
+    printf "Sorry to annoy you, this is a testing alert, please ignore it!\n%s" "$SYSSTAT" | mailx -r $MAILFROM -s `hostname -s`"/"`hostname -i`": TEST ALERT" $SMTP $MAILTO
+    exit 1;
+fi
+
+if [ "$CPU" -ge "$THCPUUSED" ] ; then
+    SYSSTAT=`sysstat "top -b -n 1 -o %CPU"`
+    printf "%s" "$SYSSTAT" | mailx -r $MAILFROM -s `hostname -s`"/"`hostname -i`": cpu high "$CPU"%" $SMTP $MAILTO
+fi
+
+if [ "$DISK" -ge "$THDISKUSED" ] ; then
+    SYSSTAT=`sysstat "top -b -n 1 -o %MEM"`
+    printf "%s" "$SYSSTAT" | mailx -r $MAILFROM -s `hostname -s`"/"`hostname -i`": root partition low "$DISK"%" $SMTP $MAILTO
+fi
+
+if [ "$MEM" -le "$THMEMFREE" ] ; then
+    SYSSTAT=`sysstat  "top -b -n 1 -o %MEM"`
+    printf "%s" "$SYSSTAT" | mailx -r $MAILFROM -s `hostname -s`"/"`hostname -i`": memory low "$MEM"%" $SMTP $MAILTO
+fi
